a15e0a5f2ab91b76909a1a8553e0761707dec756
unknown <william4800@hotmail.com>
2020-12-17 14:24:35 -0500

    Ajout gallerie image

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 $(".carte").click(function(){
-    console.log("click");
     $(".carte").css('box-shadow', '');
     $(this).css('box-shadow', '5px 0px 15px 0px #000000');
  
 });
+
+/*--------------------MENU ACCORDEON--------------------*/
 
 $(function(){
     $("#menuAccordeon").accordion({active: 0,
@@ -21,3 +22,31 @@ $(function(){
         icons: { header: "ui-icon-plus", activeHeader: "ui-icon-minus" }
     });
 })
+
+/*--------------------GALLERIE--------------------*/
+
+$(document).ready(function() {gallerie();});
+
+function gallerie(){
+  $(function(){
+
+    $('.mini').css('border', '#05386b solid 5px');
+    $('#grande-img').attr('src', $('.imageEmployee img:first').attr('src'));
+    $('.imageEmployee img:first').css('border', '#5cdb95 solid 5px');
+
+    $('.mini').click(function () {
+      // Remettre les autres miniatures à un fond blanc.
+      $('.imageEmployee .mini').css('border', '#05386b solid 5px');
+
+
+      $(this).css('border', '#5cdb95 solid 5px');
+      var nom = $(this).attr('src');
+
+      $('#grande-img').fadeOut(2000, function () {
+
+        $('#grande-img').attr('src', nom);
+        $('#grande-img').fadeIn(2000);
+      });
+    });
+  });
+}
