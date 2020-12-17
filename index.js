a3507bd25eae025d6b2cbd0e24b24f06da31cb68
unknown <william4800@hotmail.com>
2020-12-16 21:32:09 -0500

    Ajout de Nos Studios

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ $(".carte").click(function(){
     $(this).css('box-shadow', '5px 0px 15px 0px #000000');
  
 });
+
+$(function(){
+    $("#menuAccordeon").accordion({active: 0,
+        animate: {
+          duration: 2000,
+          down: {
+            easing: "easeOutBounce",
+            duration: 2000,
+          },
+        },
+        collapsible: true,
+        header: "h2",
+        heightStyle: "content", 
+        icons: { header: "ui-icon-plus", activeHeader: "ui-icon-minus" }
+    });
+})
